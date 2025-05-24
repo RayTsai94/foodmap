a84931efbb71eef02ac780c9ee88cce2d5a76039
simi0302 <92257142+simi0302@users.noreply.github.com>
2025-05-24 13:43:38 +0800

    Delete NCUFOODMAP_DJANGO directory

--- NCUFOODMAP_DJANGO/ai_recommendation/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AiRecommendationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ai_recommendation'
--- NCUFOODMAP_DJANGO/googleOauth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'googleOauth'
-
-urlpatterns = [
-    path('', views.home, name='login'),
-    path('logout/', views.logout_view, name='logout')
-]
