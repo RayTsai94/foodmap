9fa9760e00d1aa1b0e5aa8ab22180ba33f594957
RayTsai94 <raytsai615@gmail.com>
2025-05-25 10:44:01 +0800

    更新首頁、餐廳列表的網址

--- NCUFOODMAP_DJANGO/restaurants/urls.py
+++ NCUFOODMAP_DJANGO/restaurants/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('', views.restaurant_list, name='restaurant_list'),
+    path('', views.home, name='home'),
+    path('restaurants/', views.restaurant_list, name='restaurant_list'),
     path('search_suggestions/', views.search_suggestions, name='search_suggestions'),
     path('<int:pk>/', views.restaurant_detail, name='restaurant_detail'),
     path('menu-items/<int:pk>/', views.menu_item_detail, name='menu_item_detail'),
