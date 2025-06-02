ff71b8bae3c205466c63859436731cc9a3a681ec
Apoohow <hollmersi9488@gmail.com>
2025-06-02 08:09:38 +0800

    fix: 移除媒體文件配置

--- NCUFOODMAP_DJANGO/ncufoodmap_backend/urls.py
+++ NCUFOODMAP_DJANGO/ncufoodmap_backend/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,11 +30,8 @@ urlpatterns = [
     path('ai_recommendation/', include('ai_recommendation.urls')),
     path('ai_assistant/', include('ai_assistant.urls')),
     path('social/', include('social.urls')),
-    # 添加媒體文件的 URL 模式
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-# 在開發環境中添加靜態文件服務
+# 添加靜態文件的 URL 模式
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
